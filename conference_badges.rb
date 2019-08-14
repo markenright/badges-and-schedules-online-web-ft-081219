a9c3c922b557ea2fb34ef620fb27326051f4d3f2
Mark Enright <enright.mark@gmail.com>
2019-08-14 18:49:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ end
 
 def assign_rooms(array)
   new_array = []
-  array.each_with_index do |name|
-    new_array[]
+  array.each_with_index do |name, index|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
   
   end
   
