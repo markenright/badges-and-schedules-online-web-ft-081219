28d32cfa47a708de5b0b2bcbb2d07ba74f3cc8b6
Mark Enright <enright.mark@gmail.com>
2019-08-14 18:08:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  array.collect do |name| 
+    badge_maker(name)
+  end
+end
+
